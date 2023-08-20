fefe9eddd3d4330a3e1eab671d903b6fffd62f26
glowak <glowacki.admm@gmail.com>
2023-08-20 17:25:25 +0200

    add exporting sentences to tsv file

--- scripts/split_and_export_sentences.py
+++ scripts/split_and_export_sentences.py
@@ -1,7 +1,7 @@
 from coord.tools.sentences import sentence_split_exporting
 
-EXPORT_PATH = "data/txt/trankit_split_sentences_test.txt"
-DATA_PATH = "data/samples/coca-samples/text_test_sample.txt"
+EXPORT_PATH = "data/txt/trankit_split_sentences_acad_test.tsv"
+DATA_PATH = "data/samples/coca-samples/text_acad_2000.txt"
 
 if __name__ == "__main__":
-    sentence_split_exporting(DATA_PATH, EXPORT_PATH, "txt")
+    sentence_split_exporting(DATA_PATH, EXPORT_PATH, "tsv")
--- src/coord/tools/sentences.py
+++ src/coord/tools/sentences.py
@@ -2,8 +2,8 @@ from trankit import Pipeline
 from tqdm import tqdm
 
 from coord.utils.other import load_data
-from coord.tools.extract import search_for_id
-from coord.utils.data import CSVInfo
+from coord.tools.extract import search_for_id, select_conj
+from coord.utils.data import CSVInfo, TSVInfo
 from coord.utils import cleaning
 
 
@@ -11,7 +11,7 @@ def sentence_split_exporting(data_path: str,
                              export_path: str,
                              export_to: str) -> None:
 
-    p = Pipeline("english", gpu=False)
+    p = Pipeline("english", gpu=True)
     
     
     document = load_data(data_path)
@@ -19,25 +19,41 @@ def sentence_split_exporting(data_path: str,
         csv = CSVInfo(cols=["sentence", "id_trankit", "id_global"])
     elif export_to == "txt":
         file = open(export_path, "w")
+    elif export_to == "tsv":
+        tsv = TSVInfo(cols=["0", "SENT", "TYPE", "YEAR", "FILE", "PARA ID"])
 
+    id = 1
     for i in tqdm(range(0, len(document))):
         document[i] = cleaning.remove_whitespaces(cleaning.clean_text(document[i]))
         split = p.ssplit(document[i])
         clean, rm_ids = cleaning.clean_parsed(split)
         
-        for sentence in split["sentences"]:
+        for sentence in clean["sentences"]:
             if export_to == "csv":
                 csv.add_row({"sentence": sentence["text"],
                              "id_trankit": sentence["id"],
                              "id_global": "{}-{}".format(search_for_id(split), str(sentence["id"] - 1))
                              })
             elif export_to == "txt":
-                    file.write(sentence["text"] + "\n")
+                file.write(sentence["text"] + "\n")
+            elif export_to == "tsv":
+                if sentence["text"].startswith("@@"):
+                    para_id = sentence["text"]
+                else:
+                    tsv.add_row({"0": id,
+                                 "SENT": sentence["text"],
+                                 "TYPE": data_path.split("_")[1].split(".")[0],
+                                 "YEAR": data_path.split("_")[2].split(".")[0],
+                                 "FILE": data_path.split("/")[3],
+                                 "PARA ID": para_id})
+                    id += 1
                 
     if export_to == "csv":
         csv.export(export_path)
     elif export_to == "txt":
         file.close()
+    elif export_to == "tsv":
+        tsv.export(export_path)
 
 
 def depparse_sentences(pipeline: Pipeline,
